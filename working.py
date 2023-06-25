85de5336ec288ae0d6a55353f92d82ba81076cea
Bushra Khan <bushrakhan2212002@gmail.com>
2023-06-25 17:32:33 +0530

    first commit

--- working.py
+++ working.py
@@ -1,14 +1,33 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
 
 app = FastAPI()
 
 
-@app.get("/")
-def home():
-    return {"data": "testing"}
+# @app.get("/")
+# def home():
+#     return {"data": "testing"}
 
 
 
-@app.get("/about")
-def home():
-    return {"data": "about"}
+# @app.get("/about")
+# def home():
+#     return {"data": "testing"}
+
+
+inventory = {
+    1:{
+        "name": "Cheese",
+        "price": 3.45,
+        "brand": "Amul"
+    }
+}
+
+@app.get("/get-item/{item_id}") #path-parameters
+def get_item(item_id: int =  Path( description="An id of the item you'd like to view.")):
+    return inventory[item_id]
+
+#example - http://127.0.0.1:8000/get-item/1
+
+
+
+    
