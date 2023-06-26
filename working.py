fc37ce32eb6c99befc3b5143fe12596965059662
Bushra Khan <bushrakhan2212002@gmail.com>
2023-06-26 13:50:46 +0530

    commit

--- working.py
+++ working.py
@@ -1,7 +1,15 @@
-from fastapi import FastAPI, Path
+from fastapi import FastAPI, Path, Query
 from typing import Optional
+from pydantic import BaseModel
+
+
+
 app = FastAPI()
 
+class Item(BaseModel):
+    name: str
+    price: float
+    brand: Optional[str] = None
 
 # @app.get("/")
 # def home():
@@ -14,42 +22,36 @@ app = FastAPI()
 #     return {"data": "testing"}
 
 
-inventory = {
-    1:{
-        "name": "Cheese",
-        "price": 3.45,
-        "brand": "Amul"
-    }
-}
-
-
+inventory = {}
 
 #path-parameters
 @app.get("/get-item/{item_id}") 
-def get_item(item_id: int =  Path( description="An id of the item you'd like to view.")):
+def get_item(item_id: int =  Path(description="An id of the item you'd like to view.")):
     return inventory[item_id]
 
 #example - http://127.0.0.1:8000/get-item/1
 
 
-
 #query-parameters
 @app.get("/get-by-name")
-def get_item(name: Optional[str] = None):
+def get_item(name: str = Query(description="name of the item")):
     for item_id in inventory:
-        if inventory[item_id]['name'] == name:
+        print("id is",item_id)
+        if inventory[item_id].name == name:
             return inventory[item_id]
     return {"Data": "Not found"}
 
 #example - http://127.0.0.1:8000/get-by-name?name=Cheese
 
 
-#path param iwth query param
-@app.get("/get-by-name/{item_id}")
-def get_item(item_id: int, name: Optional[str] = None):
-    for item_id in inventory:
-        if inventory[item_id]['name'] == name:
-            return inventory[item_id]
-    return {"Data": "Not found"}
 
-#example - http://127.0.0.1:8000/get-by-name/1?name=Cheese
+
+
+
+@app.post('/create-item/{item_id}')
+def create_item(item_id: int, item: Item):
+    if item_id in inventory:
+        return {"Error": "Item already exists!"}
+
+    inventory[item_id] = item
+    return inventory[item_id]
